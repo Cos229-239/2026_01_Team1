0fc135ae71a62c0d72357ebbbd0db84e57b86e84
Dustin Curnutt <dustin.curnutt@gmail.com>
2026-01-28 20:39:10 -0500

    Light and Dark mode color palette added to Color.kt

--- app/src/main/java/edu/fullsail/anchor/ui/theme/Color.kt
+++ app/src/main/java/edu/fullsail/anchor/ui/theme/Color.kt
@@ -2,10 +2,65 @@ package edu.fullsail.anchor.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Dark Mode - Comfort Palette
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Background & Surfaces
+val DarkNeutralBackground = Color(0xFF2E3440)
+val DarkNeutralSurface = Color(0xFF3B4252)
+val DarkNeutralElevated = Color(0xFF434C5E)
+
+// Borders
+val DarkNeutralBorder = Color(0xFF4C566A)
+
+// Text
+val DarkTextPrimary = Color(0xFFE5E7EB)
+val DarkTextSecondary = Color(0xFFCBD5E1)
+val DarkTextDisabled = Color(0xFF9CA3AF)
+
+// Brand
+val DarkBrandPrimary = Color(0xFF2F9E97)
+val DarkBrandPrimaryMuted = Color(0xFF1F6F6B)
+
+// Semantic
+val DarkSuccess = Color(0xFF6FAE8A)
+val DarkWarning = Color(0xFFD4A24C)
+val DarkError = Color(0xFFC76A6A)
+
+// Secondary
+val DarkSecondaryBlue = Color(0xFF3A4F5C)
+val DarkSecondaryGold = Color(0xFFC7A65A)
+
+// Accent
+val DarkAccentLavender = Color(0xFFA9B4D6)
+
+
+// Light Mode - Comfort Palette
+
+// Background & Surfaces
+val LightNeutralBackground = Color(0xFFF7F9FC)
+val LightNeutralSurface = Color(0xFFFFFFFF)
+val LightNeutralElevated = Color(0xFFF0F3F8)
+
+// Borders
+val LightNeutralBorder = Color(0xFFD0D5DD)
+
+// Text
+val LightTextPrimary = Color(0xFF1F2937)
+val LightTextSecondary = Color(0xFF4B5563)
+val LightTextDisabled = Color(0xFF9CA3AF)
+
+// Brand
+val LightBrandPrimary = Color(0xFF2F9E97)
+val LightBrandPrimaryMuted = Color(0xFF1F6F6B)
+
+// Semantic
+val LightSuccess = Color(0xFF3F8F6B)
+val LightWarning = Color(0xFFB7791F)
+val LightError = Color(0xFFB45353)
+
+// Secondary
+val LightSecondaryBlue = Color(0xFF4C6B82)
+val LightSecondaryGold = Color(0xFFD4A24C)
+
+// Accent
+val LightAccentLavender = Color(0xFF7C8DB5)
